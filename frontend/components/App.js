6541b165b11ea41d1057ea615f9470efe6aa9736
LonnieFowler <97270531+LonnieFowler@users.noreply.github.com>
2022-03-08 23:57:59 -0700

    fetched todos from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import axios from 'axios'
+
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+    .then(res =>{
+     this.setState({...this.state, todos: res.data.data})
+    })
+    .catch(err => {
+     debugger 
+    })
+  }
+
+
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
+
+
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error:LoL</div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(todo => {
+              return <div key={todo.id}>{todo.name}</div>
+            })
+          }
+        </div>
+        <form id='todoForm'>
+          <input type='text' placeholder='Type your chore'></input>
+          <input type='submit'></input>
+          <button>Clear</button>
+        </form>
+      </div>
+    )
   }
 }
